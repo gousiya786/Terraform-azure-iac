e32d2a8d6332567f663f0e3a19b86d13a287f554
Fathima Gousiya Mahammad <fathimagousiya@gmail.com>
2026-01-04 23:47:57 -0500

    Delete variables.tf

--- variables.tf
+++
@@ -1,6 +0,0 @@
-variable "location" { type = string, default = "eastus" }
-variable "rg_name" { type = string }
-variable "storage_account_name" {
-  type = string
-  description = "Must be globally unique, 3-24 lowercase letters/numbers"
-}
